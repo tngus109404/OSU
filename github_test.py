fc8270883eb32e186ecb3014108bf799c6a707d4
jangsoo <jangsoo@oregonstate.edu>
2022-10-01 16:20:23 -0700

    10/01/22 msort.py

--- github_test.py
+++ github_test.py
@@ -1,2 +1,24 @@
-def test():
-    return "github!"
+from random import randint
+
+
+def qselect(i, array):
+    if len(array) > 0:
+        index = randint(0, len(array) - 1)
+        print(index)
+        array[0], array[index] = array[index], array[0]
+        pivot = array[0]
+        left = [x for x in array if x < pivot]
+        right = [x for x in array[1:] if x >= pivot]
+        array = left + [pivot] + right
+        print(array)
+        k = len(left) + 1
+        if i == k:
+            return pivot
+        elif i < k:
+            return qselect(i, left)
+        else:
+            return qselect(i - k, right)
+
+
+a=qselect(3,[6,1,8,2])
+print(a)
