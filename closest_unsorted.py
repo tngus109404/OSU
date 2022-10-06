d89eadd5655c90dea59ba44a392ce5c1c4efe585
jangsoo <jangsoo@oregonstate.edu>
2022-10-06 14:52:23 -0700

    10/01/22 closest_unsorted.py

--- closest_unsorted.py
+++ closest_unsorted.py
@@ -1,5 +1,3 @@
-import random
-
 def find(tree, x, k):
     d=[]
     for i in range(len(tree)):
@@ -28,9 +26,3 @@ def qselect(i:int, a:list)->int:
         return qselect(i - len_left - 1, right)
     else:
         return qselect(i,left)
-
-
-
-# find([4,1,3,2,7,4], 5.2, 2)#    returns   [4,4]
-# find([4,1,3,2,7,4], 6.5, 3)#	returns   [4,7,4]
-# find([5,3,4,1,6,3], 3.5, 2)#	returns   [3,4]
