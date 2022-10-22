291a68a5e81d8d93deced272c3528881e6e903ab
jangsoo <jangsoo@oregonstate.edu>
2022-10-21 22:54:38 -0700

    10/01/22 mis.py

--- mis.py
+++ mis.py
@@ -1,10 +1,10 @@
 def max_wis(array):
     la=len(array)
 
-    def fff(i,jang={-1:0, 0:0}):
-        if i not in jang:
-            jang[i] = max((array[i - 1] + fff(i - 2)), fff(i - 1))
-        return jang[i]
+    def fff(i,fibs={-1:0, 0:0}):
+        if i not in fibs:
+            fibs[i] = max((array[i - 1] + fff(i - 2)), fff(i - 1))
+        return fibs[i]
 
 
 
